9397d8937ef60a78eb8bbd195e37f0d95e9667c9
calvin6162 <calvinzhang627@gmail.com>
2023-03-11 21:02:15 +0800

    第一次提交

--- LeapYear.java
+++ LeapYear.java
@@ -1,6 +1,6 @@
 /** Class that determines whether or not a year is a leap year.
  *  @author Strike_Eagle
-aaaaaha
+aaaaha
  */
 public class LeapYear {
     public static boolean isLeapYear(int year){
